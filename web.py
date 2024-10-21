28ce73bba2ad870c2651a58544f27d68cc0c9998
Chad Aiena <caiena78@gmail.com>
2024-10-21 10:27:16 -0500

    updates

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from prometheus_client import start_http_server, Gauge, generate_latest
+from prometheus_client import Gauge, generate_latest,CollectorRegistry
 from flask import Flask, jsonify, request, Response
 import base64
 import time
@@ -10,16 +10,19 @@ from netmiko import NetMikoAuthenticationException
 
 
 # Create a single gauge metric with labels
-data_gauge = Gauge('pat_pool', 'pat_pool', ['protocol', 'interface','ip','range_start','range_end'])
 
 
-def update_metrics(ipaddress,username,password):    
+
+def update_metrics(ipaddress,username,password,registry):   
+     
+    data_gauge = Gauge('pat_pool', 'pat_pool', ['protocol', 'interface','ip','range_start','range_end'],registry=registry)       
     print(f"Firewall:{ipaddress}")
     firewall=fw.firewall(username,password)
     nat_data=firewall.getNatPool(ipaddress)
     for entry in nat_data:
-        data_gauge.labels(protocol=entry['protocol'], interface=entry['interface'],ip=entry['ip'],range_start=entry['range_start'],range_end=entry['range_end']).set(entry['size'])
-         
+        data_gauge.labels(protocol=entry['protocol'], interface=entry['interface'],ip=entry['ip'],range_start=entry['range_start'],range_end=entry['range_end']).set(entry['size'])       
+    print(f"Finished updated Firewall:{ipaddress}")   
+    
 
 
 # Flask app
@@ -51,17 +54,20 @@ def metrics():
     target = request.args.get('target')
     auth = request.headers.get('Authorization')
     username, password = base64.b64decode(auth.split(' ')[1]).decode('utf-8').split(':')
-
+    registry = CollectorRegistry()   
+    prob_success=Gauge('probe_success','prob_success',registry=registry)   
     try:
-        update_metrics(target,username,password)
+       update_metrics(target,username,password,registry)
+       prob_success.set(1)
     except NetMikoAuthenticationException:
         print("login failed")
         return authenticate()
     except Exception as e:
         print(e)
-        return jsonify({"error": e}), 400
-    loginsuccess=True
-    return Response(generate_latest(), mimetype='text/plain')
+        prob_success.set(0)
+        # return jsonify({"error": e}), 400    
+    
+    return Response(generate_latest(registry), mimetype='text/plain')
 
 if __name__ == '__main__':
     ipaddress="0.0.0.0"
